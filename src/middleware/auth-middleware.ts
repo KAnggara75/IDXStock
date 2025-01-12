20b80e2bb1a5d065b3617c215b3130c86828a98d
Call Vin <kanggara75@gmail.com>
2025-01-12 21:17:22 +0700

    fix: [auth] improve middleware response to json

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -6,11 +6,11 @@ export const authMiddleware: MiddlewareHandler = async (c, next) => {
 	const authHeader = c.req.header("Authorization");
 
 	if (!authHeader) {
-		return c.text("Authorization header is missing", 401);
+		return c.json({ errors: "Authorization header is missing" }, 401);
 	}
 
 	if (!authHeader.startsWith("Bearer ")) {
-		return c.text("Authorization header is malformed", 401);
+		return c.json({ errors: "Authorization header is malformed" }, 401);
 	}
 
 	const token = authHeader.split(" ")[1];
