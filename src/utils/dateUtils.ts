8365a3247e4158e93d14327c47dafeafb6029da1
Call Vin <kanggara75@gmail.com>
2025-01-12 19:52:19 +0700

    add: [util] date converter

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,12 +1,37 @@
+import { log } from "../config/logger.ts";
+
 export class DateUtils {
+	/**
+	 * Returns ISO-8691 Date Format from
+	 * @param dateString dateTime with format YYYY-MM-DD hh:mm:ss
+	 */
 	static toDate(dateString: string): Date {
 		try {
-			const date = new Date(dateString.replace(" ", "T"));
-			const offset = 7; // Waktu Jakarta adalah UTC+7
-			const localDate = new Date(date.getTime() + offset * 60 * 60 * 1000);
-			return localDate;
+			if (dateString.length != 19) {
+				return new Date();
+			}
+
+			return new Date(dateString.replace(" ", "T"));
 		} catch {
 			return new Date();
 		}
+	}
+
+	/**
+	 * Returns Token Expired in second
+	 * @param days from now to token Expired.
+	 */
+	static expInDays(days: number | string): number {
+		const now = Math.floor(Date.now() / 1000);
+		let day: number = 1;
+
+		if (!isNaN(Number(days))) {
+			day = Number(days);
+			log.info(`create token exp in ${day} days`);
+		}
+
+		const future: number = 3600 * 24 * day;
+
+		return now + future;
 	}
 }
---
+++ test/dateUtils.test.ts
@@ -0,0 +1,57 @@
+import { describe, expect, it } from "bun:test";
+import { DateUtils } from "../src/utils/dateUtils.ts";
+
+describe("expInDays", () => {
+	it("correct days", () => {
+		const date = DateUtils.expInDays(30);
+		const expectedDate: number = Math.floor(Date.now() / 1000) + 3600 * 24 * 30;
+
+		expect(date).toBeDefined();
+		expect(date).toBe(expectedDate);
+		expect(date).toEqual(expectedDate);
+	});
+
+	it("correct number in string", () => {
+		const date = DateUtils.expInDays("30");
+		const expectedDate: number = Math.floor(Date.now() / 1000) + 3600 * 24 * 30;
+
+		expect(date).toBeDefined();
+		expect(date).toBe(expectedDate);
+		expect(date).toEqual(expectedDate);
+	});
+
+	it("invalid number 30a", () => {
+		const date = DateUtils.expInDays("30a");
+		const expectedDate: number = Math.floor(Date.now() / 1000) + 3600 * 24;
+
+		expect(date).toBeDefined();
+		expect(date).toBe(expectedDate);
+		expect(date).toEqual(expectedDate);
+	});
+
+	it("invalid number aabb", () => {
+		const date = DateUtils.expInDays("aabb");
+		const expectedDate: number = Math.floor(Date.now() / 1000) + 3600 * 24;
+
+		expect(date).toBeDefined();
+		expect(date).toBe(expectedDate);
+		expect(date).toEqual(expectedDate);
+	});
+});
+
+describe("toDate", () => {
+	it("correct days", () => {
+		const date = DateUtils.toDate("2025-01-10 16:09:51");
+		const expectedDate: Date = new Date("2025-01-10T16:09:51.000Z");
+
+		expect(date).toBeDefined();
+		expect(date).toEqual(expectedDate);
+	});
+
+	it("incorrect days", () => {
+		const date = DateUtils.toDate("");
+		const expectedDate: Date = new Date();
+		expect(date).toBeDefined();
+		expect(date).toEqual(expectedDate);
+	});
+});
