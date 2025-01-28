5c4053f866366c6a9eeac6a14aa1f6755267d899
Call Vin <kanggara75@gmail.com>
2025-01-28 21:21:54 +0700

    chore: remove unused method

--- src/service/spreadsheet-service.ts
+++ src/service/spreadsheet-service.ts
@@ -3,33 +3,6 @@ import * as XLSX from "xlsx";
 import type { SummaryData } from "../model/summary-model.ts";
 
 export class SheetService {
-	static async toCsv(username: string, request: File): Promise<string> {
-		return await request.arrayBuffer().then((res) => {
-			const data = new Uint8Array(res);
-			const workbook = XLSX.read(data, { type: "array" });
-
-			const first_sheet_name = workbook.SheetNames[0];
-
-			const worksheet = workbook.Sheets[first_sheet_name];
-
-			const jsonData = XLSX.utils.sheet_to_json(worksheet, {
-				raw: false,
-				defval: null,
-			});
-
-			const outputCsv = `public/csv/${username}-${request.name.substring(0, request.name.indexOf(".")).split("-")[1]}.csv`;
-
-			const new_worksheet = XLSX.utils.json_to_sheet(jsonData);
-			const new_workbook = XLSX.utils.book_new();
-
-			XLSX.utils.book_append_sheet(new_workbook, new_worksheet, "csv_sheet");
-
-			XLSX.writeFile(new_workbook, outputCsv);
-
-			return outputCsv;
-		});
-	}
-
 	static async toSummaryData(request: File): Promise<SummaryData[]> {
 		return await request.arrayBuffer().then((res) => {
 			const data = new Uint8Array(res);
