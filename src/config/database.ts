2b6cfc9370abab610fa52dd33c7f98b75fde294c
Call Vin <kanggara75@gmail.com>
2025-06-14 21:36:15 +0700

    fix: redacted sql sensitive

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,25 +2,15 @@ import { log } from "./logger";
 import { Prisma, PrismaClient } from "@prisma/client";
 import { isSensitiveQuery } from "../helpers/sensitive-helper";
 
+const logLevels: Prisma.LogLevel[] = ["query", "error", "info", "warn"];
+
 export const prismaClient = new PrismaClient({
-	log: [
-		{
+	log: logLevels.map(
+		(level: Prisma.LogLevel): { emit: "event"; level: Prisma.LogLevel } => ({
 			emit: "event",
-			level: "query",
-		},
-		{
-			emit: "event",
-			level: "error",
-		},
-		{
-			emit: "event",
-			level: "info",
-		},
-		{
-			emit: "event",
-			level: "warn",
-		},
-	],
+			level,
+		})
+	),
 });
 prismaClient.$on("error", (e: Prisma.LogEvent): void => {
 	log.error(e);
@@ -33,7 +23,7 @@ prismaClient.$on("info", (e: Prisma.LogEvent): void => {
 });
 
 prismaClient.$on("query", (e: Prisma.QueryEvent): void => {
-	const sensitive = isSensitiveQuery(e.query);
+	const sensitive: boolean = isSensitiveQuery(e.query);
 	log.debug(`SQL|Query: ${sensitive ? "[SENSITIVE QUERY HIDDEN]" : e.query}`);
 	log.debug(`SQL|Params: ${sensitive ? "[REDACTED]" : e.params}`);
 	log.debug(`SQL|Duration: ${e.duration} ms`);
