5431ca9d17814a3220355d6f13735882f87adb2c
Call Vin <kanggara75@gmail.com>
2025-06-15 19:41:56 +0700

    add: FileValidation test

--- test/fileValidation.test.ts
+++ test/fileValidation.test.ts
@@ -1,23 +1,24 @@
-import { describe, expect, test } from "bun:test";
-import { FileValidation } from "../src/validation/file-validation";
 import { ZodError } from "zod";
+import type { BunFile } from "bun";
+import { describe, expect, it } from "bun:test";
+import { FileValidation } from "../src/validation/file-validation";
 
 describe("FileValidation.DOCUMENT_CHECK", (): void => {
 	const path = "./test/Stock Summary-12345678.xlsx";
-	const blob = Bun.file(path);
+	const blob: BunFile = Bun.file(path);
 
 	it("Should return true if the file name does not exist", async (): Promise<void> => {
 		const file = new File([blob], "Stock Summary-12345678.xlsx", {
 			type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
 		});
-		expect(() => FileValidation.DOCUMENT_CHECK.parse(file)).not.toThrow();
+		expect((): void => FileValidation.DOCUMENT_CHECK.parse(file)).not.toThrow();
 	});
 
 	it("Should return true if the file name does not exist", async (): Promise<void> => {
 		const file = new File([blob], "Stock-12345678.xlsx", {
 			type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
 		});
-		expect(() => FileValidation.DOCUMENT_CHECK.parse(file)).toThrow(
+		expect((): void => FileValidation.DOCUMENT_CHECK.parse(file)).toThrow(
 			"Invalid document file name"
 		);
 	});
@@ -26,8 +27,45 @@ describe("FileValidation.DOCUMENT_CHECK", (): void => {
 		const file = new File([blob], "Stock Summary-12345678.xlsx", {
 			type: "text/plain",
 		});
-		expect(() => FileValidation.DOCUMENT_CHECK.parse(file)).toThrow(
+		expect((): void => FileValidation.DOCUMENT_CHECK.parse(file)).toThrow(
 			"Invalid document file type"
 		);
 	});
 });
+
+describe("FileValidation.DOCUMENT_CHECK", (): void => {
+	it("should pass with valid XLSX file", (): void => {
+		const file: File = mockFile(
+			"Stock Summary-June.xlsx",
+			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+		);
+		expect((): void => FileValidation.DOCUMENT_CHECK.parse(file)).not.toThrow();
+	});
+
+	it("should fail with invalid file type", (): void => {
+		const file: File = mockFile("Stock Summary-June.xlsx", "application/pdf");
+		expect((): void => FileValidation.DOCUMENT_CHECK.parse(file)).toThrow(
+			ZodError
+		);
+	});
+
+	it("should fail with invalid file name", (): void => {
+		const file: File = mockFile(
+			"Report-June.xlsx",
+			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+		);
+		expect((): void => FileValidation.DOCUMENT_CHECK.parse(file)).toThrow(
+			ZodError
+		);
+	});
+
+	it("should fail if not a File instance", (): void => {
+		expect((): void => FileValidation.DOCUMENT_CHECK.parse({})).toThrow(
+			ZodError
+		);
+	});
+});
+
+function mockFile(name: string, type: string): File {
+	return new File(["dummy content"], name, { type });
+}
