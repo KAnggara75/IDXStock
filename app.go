8206f2ef9ce12c9e33df0158e73a7d867a6f283f
Call Vin <kanggara75@gmail.com>
2025-06-21 09:05:44 +0700

    fix: license

---
+++ app.go
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025 KAnggara75
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0).
+ * You are free to share and adapt the material for non-commercial purposes, provided you give appropriate credit.
+ * For more information, visit: https://creativecommons.org/licenses/by-nc/4.0/
+ *
+ * @author KAnggara75 on Sat 21/06/25 09.03
+ * @project IDXStock main
+ * https://github.com/KAnggara75/IDXStock/tree/main/app.go
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Starting IDXStock API...")
+}
