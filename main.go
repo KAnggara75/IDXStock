9aa2acf9710723e46e9fdd93ba6082e27804b553
Call Vin <kanggara75@gmail.com>
2025-06-22 00:00:31 +0700

    fix: license

---
+++ main.go
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2025 KAnggara75
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See <https://www.gnu.org/licenses/>.
+ *
+ * @author KAnggara75 on Sat 21/06/25 23.59
+ * @project IDXStock main
+ * https://github.com/KAnggara75/IDXStock/tree/main/main
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Starting IDXStock API...")
+}
