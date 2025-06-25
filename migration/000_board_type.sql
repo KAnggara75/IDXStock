a75094ce8fba982dd0ea27d96a420f5e39be41f3
Call Vin <kanggara75@gmail.com>
2025-06-25 19:37:20 +0700

    add: migration

--- migration/000_board_type.sql
+++ migration/000_board_type.sql
@@ -1,8 +1,10 @@
+DROP TYPE IF EXISTS idxstock.board CASCADE;
+
 CREATE TYPE idxstock.board AS ENUM (
+    'Main',
     'Ekonomi Baru',
     'Acceleration',
     'Development',
-    'Main',
     'Watchlist',
     'A_SERIES',
     'B_SERIES',
