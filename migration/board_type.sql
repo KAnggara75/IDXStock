192b21dd046ba3c013d1695625d073c471e8d197
Call Vin <kanggara75@gmail.com>
2025-06-25 08:31:09 +0700

    fix: stock board type

--- migration/board_type.sql
+++ migration/board_type.sql
@@ -1,9 +1,9 @@
 CREATE TYPE idxstock.board AS ENUM (
-    'Pemantauan Khusus',
-    'Utama',
-    'Pengembangan',
-    'Akselerasi',
     'Ekonomi Baru',
+    'Acceleration',
+    'Development',
+    'Main',
+    'Watchlist',
     'A_SERIES',
     'B_SERIES',
     'C_SERIES',
